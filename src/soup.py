c24e0b11e87e0e113f61d212217ca16dff23291e
LBF38 <contact.lbf38@gmail.com>
2024-01-19 11:32:59 +0100

    update soup

--- src/soup.py
+++ src/soup.py
@@ -1,8 +1,27 @@
+from src.semantics import Semantics
+
+
 class Soup:
     def __init__(self):
         pass
 
 
-class SoupSemantic:
+class SoupSemantic(Semantics):
+    def initial(self):
+        # TODO: implement this
+        return NotImplementedError
+
+    def actions(self, configuration):
+        pieces = (
+            configuration.pieces
+        )  # TODO: A verifier => récup les pieces à tout prix !
+        for piece in pieces:
+            if piece.cond(configuration):
+                return piece.lbda(configuration)
+
+
+class Piece:
     def __init__(self) -> None:
-        pass
+        self.cond = "soemthing"
+        self.lbda = "lambda syntax"
+        # TODO: implement this
