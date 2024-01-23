02ac0bd280aa6f07645a063d2de4105c3a6d1c42
LBF38 <contact.lbf38@gmail.com>
2024-01-23 10:47:03 +0100

    refactor(graph): :recycle: update rooted_graph & parent_tracer
    update rooted_graph types
    update parent_tracer and refactor to new architecture.
    Better types too

--- src/graph/rooted_graph.py
+++ src/graph/rooted_graph.py
@@ -1,15 +1,18 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Iterable
 
 
 class RootedGraph(ABC):
-    def roots(self):
+    @abstractmethod
+    def roots(self) -> Iterable:
         """
         returns the root of the graph
         :return: the roots
         """
         pass
 
-    def neighbours(self, node):
+    @abstractmethod
+    def neighbours(self, node) -> Iterable:
         """
         returns the children nodes of the graph
         :param node: the node we want to get the children from.
